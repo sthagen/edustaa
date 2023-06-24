a25bbb62d2e06263f436e9aeba0d73072c7fa0bf
Stefan Hagen <stefan@hagen.link>
2023-06-24 11:55:10 +0200

    Bumped impl version for release 2023.6.24 (packaging test) again

--- edustaa/__init__.py
+++ edustaa/__init__.py
@@ -1,7 +1,7 @@
 """Represent (Finnish: edustaa) markdown text parts as a whole in markdown, html, pdf, and troff format guided by conventions."""
 # [[[fill git_describe()]]]
-__version__ = '2023.6.24+parent.8fac4d82'
-# [[[end]]] (checksum: ec3200952e146896da6d3e4d7785be0a)
+__version__ = '2023.6.24+parent.80fb84ab'
+# [[[end]]] (checksum: 657a0f8517c2674267a2ffdbd93aa4bb)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
